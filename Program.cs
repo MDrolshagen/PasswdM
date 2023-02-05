3d7025df169d6530dfee6077086724bcba21d768
Magnus Drolshagen <114603283+MDrolshagen@users.noreply.github.com>
2023-02-05 11:10:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PasswdM // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+            Console.ReadKey();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("PaswdM");
+            Console.WriteLine("==========================");
+            Console.Write(">> Profile: ");
+            string profile = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Selected Profile: [" + profile + "]");
+
+        }
+    }
+}
